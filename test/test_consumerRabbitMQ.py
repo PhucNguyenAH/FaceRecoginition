664a9435de02ecafa875a2af06e84b87a07f94c8
Phuc <hphuc4499@gmail.com>
2021-06-17 11:33:04 +0700

    Kafka

--- test/test_consumerRabbitMQ.py
+++ test/test_consumerRabbitMQ.py
@@ -1,6 +1,7 @@
-import __init__
+from __init__ import PYTHON_PATH
 import pika, sys, os
 import json
+import pandas as pd
 from time import time
 
 # def main():
@@ -18,6 +19,9 @@ from time import time
 #     channel.start_consuming()
 count = 0
 totaltime = 0
+time_out = []
+average_out = []
+size_out = []
 def main():
     credentials = pika.PlainCredentials('phuc', '1234')
     connection = pika.BlockingConnection(pika.ConnectionParameters('192.168.1.6',5672,'/',credentials))
@@ -33,10 +37,17 @@ def main():
         count += 1
         data = json.loads(my_json)
         # print(" [x] Received %r" % data)
-        print("Image ", count, "th, Time: ", checktime-data["time"])
+        # print("Image ", count, "th, Time: ", checktime-data["time"])
         totaltime += checktime-data["time"]
-        print("-> Average ", count,"th is ", totaltime/count)
-        print()
+        # print("-> Average ", count,"th is ", totaltime/count)
+        # print("Image ", count, "th, Size: ", data["size"])
+        # print()
+        time_out.append(checktime-data["time"])
+        average_out.append(totaltime/count)
+        size_out.append(data["size"])
+        dictionary = {'time': time_out, 'average': average_out, 'size': size_out}  
+        dataframe = pd.DataFrame(dictionary) 
+        dataframe.to_csv(os.path.join(PYTHON_PATH,'rabbit.csv'))
         
     channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)
 
