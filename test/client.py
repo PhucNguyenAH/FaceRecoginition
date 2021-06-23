c58c5aee550e6607dc417012a965b37a6b42d20b
Phuc <hphuc4499@gmail.com>
2021-06-23 16:03:04 +0700

    stream

--- test/client.py
+++ test/client.py
@@ -11,21 +11,22 @@ context = zmq.Context()
 footage_socket = context.socket(zmq.PUB)
 footage_socket.bind('tcp://*:5555')
 
-SOURCE = 0
-# SOURCE = os.path.join(PYTHON_PATH, "test", "video", "test1.mp4")
+
+# SOURCE = 0
+SOURCE = os.path.join(PYTHON_PATH, "test", "video", "cam1.mp4")
 camera = cv2.VideoCapture(SOURCE)  # init the camera
 
 while True:
     grabbed, frame = camera.read()  # grab the current frame
     # frame = imutils.resize(frame, width=min(1920, frame.shape[1]))
-    frame = cv2.resize(frame, (1920, 1080))  # resize the frame
-    print("frame.shape: ",frame.shape)
+    frame = cv2.resize(frame, (854, 480))  # resize the frame
+    # print("frame.shape: ",frame.shape)
     encoded, buffer = cv2.imencode('.jpg', frame)
-    jpg_as_text = base64.b64encode(buffer)
+    jpg_as_text = base64.b64encode(buffer).decode('utf-8')
     log = {
             "photo": jpg_as_text,
             "time": time()
         }
     msg = json.dumps(log)
-    footage_socket.send(msg)
+    footage_socket.send_string(msg,zmq.NOBLOCK)
     
