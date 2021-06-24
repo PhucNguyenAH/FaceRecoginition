79259ec0db7c2bdd7644522b93fd9f5813b34b84
PhucNguyenAH <hphuc4499@gmail.com>
2021-06-24 11:32:10 +0700

    stream

--- test/client.py
+++ test/client.py
@@ -12,14 +12,14 @@ footage_socket = context.socket(zmq.PUB)
 footage_socket.bind('tcp://*:5555')
 
 
-# SOURCE = 0
-SOURCE = os.path.join(PYTHON_PATH, "test", "video", "cam1.mp4")
+SOURCE = 0
+# SOURCE = os.path.join(PYTHON_PATH, "test", "video", "cam1.mp4")
 camera = cv2.VideoCapture(SOURCE)  # init the camera
 
 while True:
     grabbed, frame = camera.read()  # grab the current frame
     # frame = imutils.resize(frame, width=min(1920, frame.shape[1]))
-    frame = cv2.resize(frame, (854, 480))  # resize the frame
+    # frame = cv2.resize(frame, (854, 480))  # resize the frame
     # print("frame.shape: ",frame.shape)
     encoded, buffer = cv2.imencode('.jpg', frame)
     jpg_as_text = base64.b64encode(buffer).decode('utf-8')
