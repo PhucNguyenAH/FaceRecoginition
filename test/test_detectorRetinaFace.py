47d5b82e5acde61c1821bbac2a3de3d096a7e1a1
PhucNguyenAH <hphuc4499@gmail.com>
2021-06-28 09:43:32 +0700

    retinaface

--- test/test_detectorRetinaFace.py
+++ test/test_detectorRetinaFace.py
@@ -1,30 +1,52 @@
-# from retinaface import RetinaFace
-import insightface
-from insightface.app import FaceAnalysis
+from __init__ import PYTHON_PATH
+
+import os
+import sys
 import cv2
-from time import time
-cam = cv2.VideoCapture(0)
+import dlib
+import glob
 
-# detector = RetinaFace
-app = FaceAnalysis(name='antelope')
-app.prepare(ctx_id=0, det_size=(640,640))
+print('PYTHON_PATH', PYTHON_PATH)
 
+from ailibs.detector.retinaface.FaceDetector import FaceDetector
+
+path = os.path.join(PYTHON_PATH, "test", "image", "4.jpg")
+output_path = os.path.join(PYTHON_PATH,"test/output/mtcnn")
+
+LOG_TIME = True
+
+image = cv2.imread(path)
+print(path, image.shape)
+
+detector = FaceDetector(log=True)
+
+# source = 0
+source = os.path.join(PYTHON_PATH, "test", "video", "test1.mp4")
+cam = cv2.VideoCapture(source)
+
+
+# dets = detector.detect(image)
+# print(dets, type(dets))
+# for d in dets:
+#     [left, top, right, bottom] = FaceDetector.get_position(d)
+#     cv2.rectangle(image, (left, top), (right, bottom), (0, 255, 0), 2)
+
+# cv2.imshow("image", image)
+# cv2.waitKey(0)
+
+frameCount = 0
 
 while True:
     ret, frame = cam.read()
     # frame = cv2.flip(frame,1)
 
-    start = time()
-    # dets = detector.detect_faces(frame)
-    dets = app.get(frame)
-    rimg = app.draw_on(frame, dets)
-    # print(time()-start)
-    # print(dets)
-    # if isinstance(dets, dict):
-    #     for det in dets.keys():
-    #         [bottom, right, top, left] = dets[det]['facial_area']
-    #         cv2.rectangle(frame, (top, left), (bottom, right), (255,255,255), 1)
-    
+    dets = detector.detect(frame)
+    for det in dets:
+        # frameCount += 1
+        [left, top, right, bottom] = FaceDetector.get_position(det)
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)
+        # face_frame = frame[top:bottom, left:right]
+        # cv2.imwrite(os.path.join(output_path,f"{frameCount}.jpg"),face_frame)
     cv2.imshow('frame', frame)
     
     if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -34,3 +56,17 @@ while True:
 cam.release()
 # Destroy all the windows
 cv2.destroyAllWindows()
+
+# input_path = os.path.join(PYTHON_PATH,"test/output/tracking")
+# for filepath in sorted(glob.glob(os.path.join(input_path,'*.jpg')), key=os.path.getmtime):
+#     image = cv2.imread(filepath)
+#     dets = detector.detect(image)
+#     # print(dets, type(dets))
+#     for d in dets:
+#         frameCount += 1
+#         [left, top, right, bottom] = FaceDetector.get_position(d)
+#         face_frame = image[top:bottom, left:right]
+#         cv2.imwrite(os.path.join(output_path,f"{frameCount}.jpg"),face_frame)
+#         # cv2.rectangle(image, (left, top), (right, bottom), (0, 255, 0), 2)
+
+#     # cv2.imshow("image", image)
